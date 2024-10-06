0c1359ed0e7a37defd3f462e57ad72c34df881ca
S B Rakesh Rath <work.sbrakeshrath@gmail.com>
2024-10-07 00:05:08 +0530

    first commit

---
+++ functions/src/src/platforms/youtube/downloadAudio.ts
@@ -0,0 +1,44 @@
+import { Context } from "telegraf";
+// import ytdl from "ytdl-core";
+
+import ytdl from "@distube/ytdl-core";
+
+export default async function downloadAudio(
+  link: string,
+  quality: string,
+  ctx: Context,
+  message_id: number
+) {
+  try {
+    const message = await ctx.reply("Please Wait... \n\n" + "Downloading audio..", {
+      reply_to_message_id: message_id,
+    });
+    // await ctx.answerCbQuery("Generating Link Please Wait....");
+
+    const res = await ytdl.getInfo(link);
+
+    const downloadLink = res.formats.find(
+      (format) => format.audioQuality === quality
+    )?.url;
+
+    // console.log(downloadLink);
+
+    if (!downloadLink) {
+      ctx.reply("Sorry we can't download this audio", {
+        reply_to_message_id: message_id,
+      });
+      return;
+    }
+
+    await ctx.deleteMessage(message.message_id);
+    
+    ctx.reply(downloadLink, {
+      reply_to_message_id: message_id,
+    });
+  } catch (error) {
+    console.log(error);
+    ctx.reply("Sorry we can't download this video", {
+      reply_to_message_id: message_id,
+    });
+  }
+}
